aacd8356ee900bf01cbaa95dd5c361cffed3a273
Harini Pavithra <harinipavithra0998@gmail.com>
2022-03-20 17:51:51 +0530

    Day 2: Operators

---
+++ Day 2: Operators.java
@@ -0,0 +1,71 @@
+Task
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
+Function Description
+Complete the solve function in the editor below.
+
+solve has the following parameters:
+
+int meal_cost: the cost of food before tip and tax
+int tip_percent: the tip percentage
+int tax_percent: the tax percentage
+Returns The function returns nothing. Print the calculated value, rounded to the nearest integer.
+
+Note: Be sure to use precise values for your calculations, or you may end up with an incorrectly rounded result.
+
+Sample Input
+
+12.00
+20
+8
+Sample Output
+
+15
+Code:
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function accepts following parameters:
+     *  1. DOUBLE meal_cost
+     *  2. INTEGER tip_percent
+     *  3. INTEGER tax_percent
+     */
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+    // Write your code here
+        double calculate_tip = tip_percent*(meal_cost/100);
+        double calculate_tax = tax_percent*(meal_cost/100);
+        System.out.println(Math.round(meal_cost+calculate_tax+calculate_tip));
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
+}
+
