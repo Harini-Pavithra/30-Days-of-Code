3483112e0e115356cfe83c58fbbe421a0ac6a4bb
Harini Pavithra <harinipavithra0998@gmail.com>
2022-03-21 20:20:31 +0530

    Day 3: Intro to Conditional Statements

---
+++ Day 3: Intro to Conditional Statements.java
@@ -0,0 +1,58 @@
+Task
+Given an integer, n, perform the following conditional actions:
+If n is odd, print Weird
+If  n is even and in the inclusive range of 2 to 5 , print Not Weird
+If n is even and in the inclusive range of  6 to 20, print Weird
+If n is even and greater than 20, print Not Weird
+Input Format
+
+A single line containing a positive integer, n.
+
+Output Format
+
+Print Weird if the number is weird; otherwise, print Not Weird.
+
+Code:
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        if(N%2!=0)
+        {
+            System.out.println("Weird");
+        }
+        else if(N%2==0)
+        {
+            if(N>=2 && N<=5)
+            {
+            System.out.println("Not Weird");
+            }
+            if(N>=6 && N<=20)
+            {
+                System.out.println("Weird");
+            }
+            if(N>20)
+            {
+                System.out.println("Not Weird");
+            }
+        }
+    }
+}
+
