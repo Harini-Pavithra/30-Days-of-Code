d7ebd25c9051b98d34d2d2dec2612e0fb8846479
Harini Pavithra <harinipavithra0998@gmail.com>
2022-03-22 21:58:01 +0530

    Day 4: Class vs. Instance

---
+++ Day 4: Class vs. Instance.java
@@ -0,0 +1,60 @@
+Objective
+In this challenge, we're going to learn about the difference between a class and an instance; because this is an Object Oriented concept, 
+it's only enabled in certain languages. Check out the Tutorial tab for learning materials and an instructional video!
+
+  Code:
+import java.io.*;
+import java.util.*;
+
+public class Person {
+    private int age;    
+  
+    public Person(int initialAge) {
+        // Add some more code to run some checks on initialAge
+          if(initialAge < 0)
+          {
+              age = 0;
+              System.out.println("Age is not valid, setting age to 0.");
+          }
+          else{
+              age = initialAge;
+          }
+    }
+
+    public void amIOld() {
+        // Write code determining if this person's age is old and print the correct statement:
+        if(age<13)
+        {
+            System.out.println("You are young.");
+        }  
+        else if(age>=13 && age<18)
+        {
+            System.out.println("You are a teenager.");
+        }
+        else
+        {
+        System.out.println("You are old.");
+        }
+    }
+
+    public void yearPasses() {
+        // Increment this person's age.
+          age++;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int age = sc.nextInt();
+            Person p = new Person(age);
+            p.amIOld();
+            for (int j = 0; j < 3; j++) {
+                p.yearPasses();
+            }
+            p.amIOld();
+            System.out.println();
+        }
+        sc.close();
+    }
+}
