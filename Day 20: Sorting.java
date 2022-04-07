faa0538cd4364fc4e23979f806010c7beae4a8cc
Harini Pavithra <harinipavithra0998@gmail.com>
2022-04-07 12:32:59 +0530

    Day 20: Sorting

---
+++ Day 20: Sorting.java
@@ -0,0 +1,132 @@
+
+Objective
+Today, we're discussing a simple sorting algorithm called Bubble Sort. Check out the Tutorial tab for learning materials and an instructional video!
+
+Consider the following version of Bubble Sort:
+
+for (int i = 0; i < n; i++) {
+    // Track number of elements swapped during a single array traversal
+    int numberOfSwaps = 0;
+    
+    for (int j = 0; j < n - 1; j++) {
+        // Swap adjacent elements if they are in decreasing order
+        if (a[j] > a[j + 1]) {
+            swap(a[j], a[j + 1]);
+            numberOfSwaps++;
+        }
+    }
+    
+    // If no elements were swapped during a traversal, array is sorted
+    if (numberOfSwaps == 0) {
+        break;
+    }
+}
+Task
+Given an array, a, of size n distinct elements, sort the array in ascending order using the Bubble Sort algorithm above. Once sorted, print the following 3 lines:
+
+Array is sorted in numSwaps swaps.
+where numSwaps is the number of swaps that took place.
+First Element: firstElement
+where firstElement is the first element in the sorted array.
+Last Element: lastElement
+where lastElement is the last element in the sorted array.
+Hint: To complete this challenge, you will need to add a variable that keeps a running tally of all swaps that occur during execution.
+
+Example
+a = [4, 3, 1, 2]
+
+original a: 4 3 1 2
+round 1  a: 3 1 2 4 swaps this round: 3
+round 2  a: 1 2 3 4 swaps this round: 2
+round 3  a: 1 2 3 4 swaps this round: 0
+In the first round, the 4 is swapped at each of the 3 comparisons, ending in the last position. In the second round, the 3 is swapped at 2 of the 3 comparisons. Finally, in the third round, no swaps are made so the iterations stop. The output is the following:
+
+Array is sorted in 5 swaps.
+First Element: 1
+Last Element: 4
+Input Format
+The first line contains an integer, n, the number of elements in array a.
+The second line contains n space-separated integers that describe a[0], a[1], . . .a[n – 1].
+
+Constraints
+2 <= n <= 600
+1 <= a[i] <= 2 x106, where 0 <= i < n
+Output Format
+Print the following three lines of output:
+
+Array is sorted in numSwaps swaps.
+where numSwaps is the number of swaps that took place.
+First Element: firstElement
+where firstElement is the first element in the sorted array.
+Last Element: lastElement
+where lastElement is the last element in the sorted array.
+Sample Input 0
+
+3
+1 2 3
+Sample Output 0
+
+Array is sorted in 0 swaps.
+First Element: 1
+Last Element: 3
+Sample Input 1
+
+3
+3 2 1
+Sample Output 1
+
+Array is sorted in 3 swaps.
+First Element: 1
+Last Element: 3
+Coding:
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        int[] a = new int[n];
+        for(int i =0;i<n;i++)
+        {
+            a[i]  = scan.nextInt();
+        }
+        
+        int numberOfSwaps = 0;
+        // Write your code here
+        for (int i = 0; i < n; i++) {
+    // Track number of elements swapped during a single array traversal
+    for (int j = 0; j < n - 1; j++) {
+        // Swap adjacent elements if they are in decreasing order
+        if (a[j] > a[j + 1]) {
+            int temp = a[j];
+            a[j] = a[j+1];
+            a[j+1] = temp;
+            numberOfSwaps++;
+        }
+    }
+    // If no elements were swapped during a traversal, array is sorted
+    if (numberOfSwaps == 0) {
+        break;
+    }
+}
+System.out.println("Array is sorted in "+numberOfSwaps +" swaps.");
+System.out.println("First Element: "+a[0]);
+System.out.println("Last Element: "+a[n-1]);
+
+        scan.close();
+    }
+}
